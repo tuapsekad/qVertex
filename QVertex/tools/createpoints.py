b0f95259c275e3f27ae4c2d8190ce308a199c2eb
Dzgoev <reg@kadsoft.ru>
2014-08-28 17:19:20 +0400

    Генерация точек во временный слой (memory).

--- QVertex/tools/createpoints.py
+++ QVertex/tools/createpoints.py
@@ -1,65 +1,96 @@
 # -*- coding: utf-8 -*-
 __author__ = 'filippov'
 
-from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from qgis.core import *
+
+numLayer = 0
 
 
 class CreatePoints():
     def __init__(self, iface):
         self.iface = iface
+        self.numPoint = 0
 
         if (self.iface.mapCanvas().currentLayer() is not None) \
                 and (self.iface.mapCanvas().currentLayer().selectedFeatures() is not None):
             self.selection = self.iface.mapCanvas().currentLayer().selectedFeatures()
+            self.workLayer = self.iface.activeLayer()
+            self.CrsSelectLayer = self.iface.mapCanvas().currentLayer().crs().authid()
+            self.UriLayer = "Point?crs=%s&field=name:string(8)&index=yes" % self.CrsSelectLayer
+            self.targetLayer = self.createNewLayer()
         else:
-            QMessageBox.warning(self.iface.mainWindow(), 'Нет выбранных объектов', QMessageBox.Ok, QMessageBox.Ok)
+            QMessageBox.warning(self.iface.mainWindow(), u"Внимание", u"Нет выбранных объектов", QMessageBox.Ok)
             return False
 
-        self.layermap = QgsMapLayerRegistry.instance().mapLayers()
-        for name, layer in self.layermap.iteritems():
-            #QMessageBox.information(self.iface.mainWindow(), layer.name(), str())
-            if layer.type() == QgsMapLayer.VectorLayer and layer.name() == u"Точки":
-                if layer.isValid():
-                    self.targetLayer = layer
-                else:
-                    self.selection = None
 
     def Create(self):
         if (self.selection is None):
             return False
         self.targetLayer.startEditing()
-        numPoint = 0
-        iter = 0
+
         for every in self.selection:
             geom = every.geometry()
-            if geom.isMultipart():
-                polygons = geom.asMultiPolygon()
-                for polygone in polygons:
-                    self.numberRing = 0
-                    for ring in polygone:
-                        iter = 0
-                        self.numberRing += 1
-                        for i in ring:
-                            if iter < len(ring)-1:
-                                numPoint += 1
-                                self.createPointOnLayer(i, numPoint)
-                            iter += 1
+            self.createP(geom)
+            for f in self.workLayer.getFeatures():
+                if geom.contains(f.geometry()):
+                    if f.geometry().exportToWkt() != geom.exportToWkt():
+                        self.createP(f.geometry())
 
-            else:
+        self.targetLayer.commitChanges()
+        self.removeDuplicate(self.targetLayer)
+
+
+    def removeDuplicate(self, layer):
+        layer.startEditing()
+
+        geometries = []
+        for feature in layer.getFeatures():
+            geometries.append(feature.geometry().exportToWkt())
+
+        for x in range(0, len(geometries) - 1):
+            if geometries[x] != None:
+                for y in range(x + 1, len(geometries)):
+                    if geometries[x] == geometries[y]:
+                        geometries[y] = None
+
+        for index, feature in enumerate(layer.getFeatures()):
+            layer.deleteFeature(feature.id())
+            if geometries[index] != None:
+                layer.addFeature(feature)
+
+        for index, feature in enumerate(layer.getFeatures()):
+            layer.changeAttributeValue(feature.id(), 0, u'н' + str(index + 1))
+
+        layer.commitChanges()
+
+
+    def createP(self, geom):
+        if geom.isMultipart():
+            polygons = geom.asMultiPolygon()
+            for polygone in polygons:
                 self.numberRing = 0
-                rings = geom.asPolygon()
-                for ring in rings:
+                for ring in polygone:
                     iter = 0
                     self.numberRing += 1
                     for i in ring:
-                        if iter < len(ring)-1:
-                            numPoint += 1
-                            self.createPointOnLayer(i, numPoint)
+                        if iter < len(ring) - 1:
+                            self.numPoint += 1
+                            self.createPointOnLayer(i, self.numPoint)
                         iter += 1
 
-        self.targetLayer.commitChanges()
+        else:
+            self.numberRing = 0
+            rings = geom.asPolygon()
+            for ring in rings:
+                iter = 0
+                self.numberRing += 1
+                for i in ring:
+                    if iter < len(ring) - 1:
+                        self.numPoint += 1
+                        self.createPointOnLayer(i, self.numPoint)
+                    iter += 1
+
 
     def createPointOnLayer(self, point, name):
         feature = QgsFeature()
@@ -69,3 +100,22 @@ class CreatePoints():
         self.targetLayer.dataProvider().addFeatures([feature])
         del feature
         return True
+
+
+    def createNewLayer(self):
+        layer = QgsVectorLayer(self.UriLayer, u'.Точки' + str(self.getNumLayers()), "memory")
+
+        layer.startEditing()
+        layer.setCustomProperty("labeling", "pal")
+        layer.setCustomProperty("labeling/enabled", "true")
+        layer.setCustomProperty("labeling/fieldName", "name")
+        layer.commitChanges()
+
+        QgsMapLayerRegistry.instance().addMapLayers([layer])
+        return layer
+
+
+    def getNumLayers(self):
+        global numLayer
+        numLayer += 1
+        return numLayer
